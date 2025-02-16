47b58e347ad45eb63f9beab83d50ecae76f8e578
Michal Shelenberg <email@michal.sh>
2025-02-16 15:41:48 +0100

    Add Footer component and enhance Home page layout with new section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
